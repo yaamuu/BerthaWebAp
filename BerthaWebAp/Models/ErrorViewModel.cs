f12ca56a4350477e1298e1f1bb53b3d7b857b038
Marcin Sieradzki <32650308+yaamuu@users.noreply.github.com>
2018-10-31 10:56:52 +0100

    Add project files.

---
+++ BerthaWebAp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BerthaWebAp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
